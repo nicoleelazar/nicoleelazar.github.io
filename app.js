c1277d377f3e92f20704847b66b6b47a06ae4ca4
Nicole Elazar <nicole.elazar@gmail.com>
2020-05-31 17:24:57 +0200

    text amendments

--- app.js
+++ app.js
@@ -1,157 +1,144 @@
-const copyMe = document.querySelector('.copy');
-const emailButton = document.querySelector('.email-btn');
-const emailArea = document.getElementById('hidden-content');
-const year = document.getElementById('year');
-const modals = document.querySelectorAll('.modal');
-const modalBacking = document.getElementById('modal-backing');
-const navBar = document.querySelector('.nav-bar');
-const target = document.querySelector('.scroll');
+const copyMe = document.querySelector(".copy");
+const emailButton = document.querySelector(".email-btn");
+const emailArea = document.getElementById("hidden-content");
+const year = document.getElementById("year");
+const modals = document.querySelectorAll(".modal");
+const modalBacking = document.getElementById("modal-backing");
+const navBar = document.querySelector(".nav-bar");
+const target = document.querySelector(".scroll");
 
-
-const burger = document.getElementById('burger-check');
-const mobileMenu = document.querySelectorAll('.nav-mobile-item');
-
+const burger = document.getElementById("burger-check");
+const mobileMenu = document.querySelectorAll(".nav-mobile-item");
 
 // Update footer year
 function updateYear() {
-    year.innerHTML = ` 2019-${new Date().getFullYear()} `;
+    year.innerHTML = ` ${new Date().getFullYear()} `;
 }
-
 
 // start at home page on re-load
 $(document).ready(function () {
     $(this).scrollTop(0);
-    $('#burger-check').prop('checked', false);
+    $("#burger-check").prop("checked", false);
 
     //DISABLE VIDEO FOR MOBILE
     let screenWidth = $(window).width();
     if (screenWidth > 850) {
-        $('#hero-video').attr('autoplay', 'autoplay');
+        $("#hero-video").attr("autoplay", "autoplay");
     } else {
-        let videoPoster = "/assets/poster.png"
-        $('.home-poster-mobile').css({
-            'background-image': 'url(' + videoPoster + ')',
-            'display': 'block'
+        let videoPoster = "/assets/poster.png";
+        $(".home-poster-mobile").css({
+            "background-image": "url(" + videoPoster + ")",
+            display: "block",
         });
     }
 
     //Year in footer
     updateYear();
-
 });
 
-
-
 // SMOOTH SCROLL - cross browser
-$('.nav-bar a').on('click', function (e) {
-    if (this.hash !== '') {
+$(".nav-bar a").on("click", function (e) {
+    if (this.hash !== "") {
         e.preventDefault();
 
         const hash = this.hash;
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 600);
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top,
+            },
+            600
+        );
     }
-})
+});
 
 // NAV BAR - fade in color on scroll
 function scrollAll() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-        navBar.style.background = 'rgba(255, 255, 255, 0.8)';
+    if (
+        document.body.scrollTop > 150 ||
+        document.documentElement.scrollTop > 150
+    ) {
+        navBar.style.background = "rgba(255, 255, 255, 0.8)";
     } else {
-        navBar.style.background = 'rgba(255, 255, 255, 0)';
+        navBar.style.background = "rgba(255, 255, 255, 0)";
     }
 }
-window.addEventListener('scroll', scrollAll);
-
-
+window.addEventListener("scroll", scrollAll);
 
 //parallax
 function parallax() {
-
     let scrolled = window.pageYOffset;
     let rate = scrolled * -0.8;
 
-    target.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
+    target.style.transform = "translate3d(0px, " + rate + "px, 0px)";
 }
-window.addEventListener('scroll', parallax);
-
-
-
+window.addEventListener("scroll", parallax);
 
 // OPEN MODALS
 function openModal(idName) {
     const modal = document.getElementById(idName);
-    modal.style.display = 'block';
+    modal.style.display = "block";
 
     //hide the body's scroll bar
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
 
-    modalBacking.style.display = 'block';
+    modalBacking.style.display = "block";
 }
-
 
 // CLOSE MODALS
 function closeModal(idName) {
     const modal = document.getElementById(idName);
-    modal.style.display = 'none';
+    modal.style.display = "none";
 
     //show the body's scroll bar
-    document.body.style.overflow = 'auto';
+    document.body.style.overflow = "auto";
 
-    modalBacking.style.display = 'none';
-
+    modalBacking.style.display = "none";
 }
-
-
 
 // HOVER EFFECTS FOR 'EMAIL ME' DIV
 function hovering() {
     //show message on hover
-    copyMe.innerHTML = 'Copy to Clipboard';
-    copyMe.style.color = 'var(--color-grey-light)';
+    copyMe.innerHTML = "Copy to Clipboard";
+    copyMe.style.color = "var(--color-grey-light)";
 }
-emailButton.addEventListener('mouseover', hovering);
+emailButton.addEventListener("mouseover", hovering);
 
 function notHovering() {
-    copyMe.classList.remove('fade-copy');
-    copyMe.innerHTML = '';
+    copyMe.classList.remove("fade-copy");
+    copyMe.innerHTML = "";
 }
-emailButton.addEventListener('mouseout', notHovering);
-
+emailButton.addEventListener("mouseout", notHovering);
 
 // SHOW EMAIL ADDRESS ON CLICK
 function revealEmail() {
     emailArea.innerHTML = construction(false);
 
-    copyMe.innerHTML = 'Copied!';
-    copyMe.classList.add('fade-copy');
+    copyMe.innerHTML = "Copied!";
+    copyMe.classList.add("fade-copy");
 
     //run copy to clipboard
     copyToClipboard();
 }
-emailButton.addEventListener('click', revealEmail);
+emailButton.addEventListener("click", revealEmail);
 
-
-// CONSTRUCT EMAIL ADDRESS 
+// CONSTRUCT EMAIL ADDRESS
 function construction(argument = true) {
     let user;
     let domain;
 
     if (argument) {
-        domain = 'oops' + '.' + 'oops';
-        user = 'oops';
+        domain = "oops" + "." + "oops";
+        user = "oops";
     } else {
-        domain = 'gmail' + '.' + 'com';
-        user = 'nicole' + '.' + 'elazar';
+        domain = "gmail" + "." + "com";
+        user = "nicole" + "." + "elazar";
     }
-    return (user + "@" + domain);
+    return user + "@" + domain;
 }
-
 
 function copyToClipboard() {
     //temporarily create an input field to copy from
-    let tempField = document.createElement('input');
+    let tempField = document.createElement("input");
     document.body.appendChild(tempField);
     tempField.value = construction(false);
     tempField.select();
@@ -159,12 +146,11 @@ function copyToClipboard() {
     tempField.remove();
 }
 
-
 // MOBILE NAV - CLOSE ON CLICK
 function closeMobileNav() {
     burger.checked = false;
 }
 
 for (let item of mobileMenu) {
-    item.addEventListener('click', closeMobileNav);
+    item.addEventListener("click", closeMobileNav);
 }
